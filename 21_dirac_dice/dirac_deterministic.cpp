6ad35fa77509649f6ecdc1a304c735a0fb364417
Jonathan Dursi <jonathan@dursi.ca>
2021-12-21 10:42:40 -0500

    Dec 21

---
+++ 21_dirac_dice/dirac_deterministic.cpp
@@ -0,0 +1,136 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <regex>
+
+class Die {
+    public:
+        Die(int sides) : sides(sides), nrolls(0) {};
+        virtual int roll() = 0;
+        virtual int num_rolls() const { return nrolls; }
+        int num_sides() const { return sides; }
+    protected:
+        void increment_roll() { nrolls++; }
+        int sides;
+        int nrolls;
+};
+
+class DeterministicDie : public Die {
+    public:
+        DeterministicDie(int sides, int value) : Die(sides), value(value) {}
+        int roll() override {
+            Die::increment_roll();
+            int result = value;
+            value = (value % sides) + 1;
+            return result;
+        };
+    private:
+        int value;
+};
+
+class DiracDice { 
+    public:
+        DiracDice(Die* die, std::vector<int> starting_positions) : die(die) {
+            for (int i=0; i<nplayers; i++) {
+                player_positions[i] = starting_positions[i];
+                player_scores[i] = 0;
+            }
+        }
+
+        void turn() {
+            int move = 0;
+            for (int i=0; i<nrolls_per_turn; i++)
+                move += die->roll();
+
+            player_positions[player_turn] = (player_positions[player_turn] - 1 + move) % board_size + 1;
+            player_scores[player_turn] += player_positions[player_turn];
+
+            player_turn = (player_turn + 1) % nplayers;
+        }
+
+        bool won() {
+            for (int i=0; i<nplayers; i++)
+                if (player_scores[i] >= max_score)
+                    return true;
+            return false;
+        }
+
+        long int game_score() {
+            int loser = 0;
+            for (int i=1; i<nplayers; i++)
+                if (player_scores[i] < player_scores[loser])
+                    loser = i;
+
+            return player_scores[loser]*(die->num_rolls());
+        }
+
+        std::string to_string() const {
+            std::stringstream result;
+            for (int i=0; i<nplayers; i++)
+                result << "Player " << i+1 << " on space " << player_positions[i] << " with score " << player_scores[i] << "\n";
+            return result.str();
+        }
+
+    private:
+        static const int nplayers = 2;
+        static const int board_size = 10;
+        static const int max_score = 1000;
+        static const int nrolls_per_turn = 3;
+        int player_positions[nplayers];
+        int player_scores[nplayers];
+        int player_turn = 0;
+        Die *die;
+};
+
+std::vector<int> starting_positions(std::ifstream& input) {
+    std::vector<int> player;
+    std::vector<int> start;
+    int maxplayers = -1;
+
+    std::string line;
+    const std::regex target_re("Player (\\d+) starting position: (\\d+)");
+    std::smatch smatches;
+
+    while (std::getline(input, line)) {
+        if (!std::regex_match(line, smatches, target_re))
+            continue;
+
+        int player_num = std::stoi(smatches[1])-1;
+        player.push_back(player_num);
+        if (player_num > maxplayers)
+            maxplayers = player_num;
+
+        start.push_back(std::stoi(smatches[2]));
+    }
+
+    std::vector<int> result = std::vector<int>(maxplayers+1, 0);
+    for (int player_num=0; player_num <= maxplayers; player_num++) {
+        for (int i=0; i<player.size(); i++)
+            if (player[i] == player_num)
+                result[player_num] = start[i];
+    }
+
+    return result;
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
+        return 1;
+    }
+
+    std::ifstream input(argv[1]);
+    auto starts = starting_positions(input);
+
+    DeterministicDie die(100, 1);
+    DiracDice game(&die, starts);
+
+    while (!game.won()) {
+        game.turn();
+    }
+
+    std::cout << "Part 1:" << std::endl;
+    std::cout << "      Game score: " << game.game_score() << std::endl;
+}
---
+++ 21_dirac_dice/dirac_quantum.cpp
@@ -0,0 +1,188 @@
+#include <vector>
+#include <map>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <regex>
+
+const int nplayers = 2;
+
+struct UniverseState {
+    short int positions[nplayers];
+    short int scores[nplayers];
+
+    // need this comparison operator so we can have a map of them
+    bool operator<(const UniverseState& other) const {
+        for (int i=0; i<nplayers; i++) {
+            if (positions[i] < other.positions[i])
+                return true;
+            if (scores[i] < other.scores[i])
+                return true;
+            if (positions[i] > other.positions[i])
+                return false;
+            if (scores[i] > other.scores[i])
+                return false;
+        }
+        return false;
+    }
+    UniverseState& operator=(const UniverseState& other) {
+        for (int i=0; i<nplayers; i++) {
+            positions[i] = other.positions[i];
+            scores[i] = other.scores[i];
+        }
+        return *this;
+    }
+};
+
+class MultiverseSimulator {
+    public:
+        static const int board_size = 10;
+        static const int max_score = 21;
+
+        MultiverseSimulator(const std::vector<long int>& die_rolls, const std::vector<short int>& starting_positions) : die_roll_splits(die_rolls) {
+            UniverseState initial_state;
+            for (int i=0; i<nplayers; i++) {
+                initial_state.positions[i] = starting_positions[i];
+                initial_state.scores[i] = 0;
+                nwins[i] = 0l;
+            }
+            Universes[initial_state] = 1l;
+            nuniverses_playing = 1l;
+        }
+
+        void turn() {
+            std::map<UniverseState, long int> new_universes;
+            long int still_playing = 0;
+            for (auto& [universe, count] : Universes) {
+                for (short int move=0; move<die_roll_splits.size(); move++) {
+                    long int n_outcomes = die_roll_splits[move];
+                    if (n_outcomes == 0)
+                        continue;
+
+                    long int nuniverses = count * n_outcomes;
+
+                    UniverseState new_state = universe;
+                    new_state.positions[player_turn] = (new_state.positions[player_turn] - 1 + move) % board_size + 1;
+                    new_state.scores[player_turn] += new_state.positions[player_turn];
+
+                    if (new_state.scores[player_turn] >= max_score) {
+                        // don't create further new universes if the player has won - game ends
+                        nwins[player_turn] += nuniverses;
+                    } else {
+                        // create new universes
+                        new_universes[new_state] += nuniverses;
+                        still_playing += nuniverses;
+                    }
+                }
+            }
+            player_turn = (player_turn + 1) % nplayers;
+            Universes = new_universes;
+            nuniverses_playing = still_playing;
+        }
+
+        bool done() {
+            return nuniverses_playing == 0;
+        }
+
+        std::string to_string() {
+            std::stringstream result;
+            for (auto& [universe, count] : Universes) {
+                result << count << " universes with state: " << std::endl;
+                for (int i=0; i<nplayers; i++)
+                    result << "    Player " << i+1 << " on space " << universe.positions[i] << " with score " << universe.scores[i] << std::endl;
+            }
+            return result.str();
+        }
+
+        std::vector<long int> get_wins() {
+            std::vector<long int> result;
+            for (int i=0; i<nplayers; i++)
+                result.push_back(nwins[i]);
+            return result;
+        }
+
+    private:
+        std::map<UniverseState, long int> Universes;
+        long int nuniverses_playing = 0;
+        long int nwins[nplayers];
+        int player_turn = 0;
+        std::vector<long int> die_roll_splits;
+};
+
+// all possible outcomes and counts
+std::vector<long int>die_rolls(const int nsides, const int nrolls) {
+    const int maxscore = nsides * nrolls + 1;
+
+    if (nrolls == 1) {
+        std::vector<long int> result(maxscore, 1);
+        result[0] = 0;
+        return result;
+    }
+
+    std::vector<long int> result(maxscore, 0);
+    std::vector<long int> prev_result = die_rolls(nsides, nrolls - 1);
+
+    for (int side=1; side <= nsides; side++) {
+        for (int score=0; score < prev_result.size(); score++) {
+            result[score + side] += prev_result[score];
+        }
+    }
+
+    return result;
+}
+
+std::vector<short int> starting_positions(std::ifstream& input) {
+    std::vector<short int> player;
+    std::vector<short int> start;
+    int maxplayers = -1;
+
+    std::string line;
+    const std::regex target_re("Player (\\d+) starting position: (\\d+)");
+    std::smatch smatches;
+
+    while (std::getline(input, line)) {
+        if (!std::regex_match(line, smatches, target_re))
+            continue;
+
+        int player_num = std::stoi(smatches[1])-1;
+        player.push_back(player_num);
+        if (player_num > maxplayers)
+            maxplayers = player_num;
+
+        start.push_back(std::stoi(smatches[2]));
+    }
+
+    std::vector<short int> result = std::vector<short int>(maxplayers+1, 0);
+    for (int player_num=0; player_num <= maxplayers; player_num++) {
+        for (int i=0; i<player.size(); i++)
+            if (player[i] == player_num)
+                result[player_num] = start[i];
+    }
+
+    return result;
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
+        return 1;
+    }
+
+    std::ifstream input(argv[1]);
+    auto starts = starting_positions(input);
+    auto roll_possibilities = die_rolls(3, 3);
+
+    auto mvs = MultiverseSimulator(roll_possibilities, starts);
+    while (!mvs.done()) 
+        mvs.turn();
+
+    std::cout << mvs.to_string() << std::endl;
+    auto wins = mvs.get_wins();
+    for (int i=0; i<wins.size(); i++)
+        std::cout << wins[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "Part 2:" << std::endl;
+    std::cout << "      " << *std::max_element(wins.begin(), wins.end()) << std::endl;
+}
